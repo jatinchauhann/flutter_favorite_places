15b945ee64aa191679ee6edfae172b45c209fcaf
Jatin Chauhan <jtnchauhan24@gmail.com>
2023-05-28 01:58:50 +0530

    adding image in the place details screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -22,6 +22,9 @@ class PlacesScreen extends ConsumerWidget {
                 icon: const Icon(Icons.add))
           ],
         ),
-        body: PlacesList(places: userPlaces));
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: PlacesList(places: userPlaces),
+        ));
   }
 }
