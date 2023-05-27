768725cf72e1b028979d7961105fe26eec4d3ac9
Jatin Chauhan <jtnchauhan24@gmail.com>
2023-05-28 01:44:16 +0530

    using image_picker to add images to the add item screen

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,10 +12,38 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+        onPressed: _takePicture);
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
           border: Border.all(
@@ -22,10 +52,7 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-          icon: const Icon(Icons.camera),
-          label: const Text('Take Picture'),
-          onPressed: _takePicture),
+      child: content,
     );
   }
 }
