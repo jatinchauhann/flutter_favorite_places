3ae03b8a04438b14f4167b9b89d43fa76c319b3f
Jatin Chauhan <jtnchauhan24@gmail.com>
2023-06-04 23:53:01 +0530

    adding location package and getting user's location

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_favorite_places/providers/user_places.dart';
 import 'package:flutter_favorite_places/widgets/image_input.dart';
+import 'package:flutter_favorite_places/widgets/location_input.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
@@ -58,6 +59,10 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               _selectedImage = image;
             },
           ),
+          const SizedBox(
+            height: 16,
+          ),
+          LocationInput(),
           const SizedBox(
             height: 16,
           ),
